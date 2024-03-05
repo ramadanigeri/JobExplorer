80bb8a0392f98d5f8d2bcf1602193db11ae19e4d
Ramadani, Geri <ramadanigeri@gmail.com>
2024-03-05 15:04:37 +0100

    Configure header left & right with touchable opacity

--- app/index.js
+++ app/index.js
@@ -20,10 +20,10 @@ const Home = () => {
           headerStyle: { backgroundColor: COLORS.lightWhite },
           headerShadowVisible: false,
           headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.menu} dimention="60%" />
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl={images.profile} dimention="100%" />
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
           ),
           headerTitle: '',
         }}
