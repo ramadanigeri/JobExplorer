d3d6eafa9d8c546207173d02eca143d04f6ed41e
Geri Ramadani <ramadanigeri@gmail.com>
2024-03-06 17:01:44 +0100

    Create Job Details + Home Redirect + Welcome page update

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,64 +1,142 @@
-import { useState } from 'react'
-import { useRouter } from 'expo-router'
+import React, { useEffect, useState } from 'react'
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  FlatList,
   ActivityIndicator,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  View,
 } from 'react-native'
+import { Stack, useRouter, useSearchParams } from 'expo-router'
+import { Text, SafeAreaView } from 'react-native'
+import axios from 'axios'
 
-import styles from './popularjobs.style'
-import { COLORS, SIZES } from '../../../constants'
-import PopularJobCard from '../../common/cards/popular/PopularJobCard'
-import useFetch from '../../../hook/useFetch'
+import { ScreenHeaderBtn, NearbyJobCard } from '../../components'
+import { COLORS, icons, SIZES } from '../../constants'
+import styles from '../../styles/search'
 
-const Popularjobs = () => {
+const JobSearch = () => {
+  const params = useSearchParams()
   const router = useRouter()
-  const { data, isLoading, error } = useFetch('search', {
-    query: 'React developer',
-    num_pages: '1',
-  })
 
-  const [selectedJob, setSelectedJob] = useState()
+  const [searchResult, setSearchResult] = useState([])
+  const [searchLoader, setSearchLoader] = useState(false)
+  const [searchError, setSearchError] = useState(null)
+  const [page, setPage] = useState(1)
 
-  const handleCardPress = (item) => {
-    router.push(`/job-details/${item.job_id}`)
-    setSelectedJob(item.job_id)
+  const handleSearch = async () => {
+    setSearchLoader(true)
+    setSearchResult([])
+
+    try {
+      const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/search`,
+        headers: {
+          'X-RapidAPI-Key': '',
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+        },
+        params: {
+          query: params.id,
+          page: page.toString(),
+        },
+      }
+
+      const response = await axios.request(options)
+      setSearchResult(response.data.data)
+    } catch (error) {
+      setSearchError(error)
+      console.log(error)
+    } finally {
+      setSearchLoader(false)
+    }
   }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popular jobs</Text>
-        <TouchableOpacity>
-          <Text style={styles.headerBtn}>Show all</Text>
-        </TouchableOpacity>
-      </View>
+  const handlePagination = (direction) => {
+    if (direction === 'left' && page > 1) {
+      setPage(page - 1)
+      handleSearch()
+    } else if (direction === 'right') {
+      setPage(page + 1)
+      handleSearch()
+    }
+  }
 
-      <View style={styles.cardsContainer}>
-        {isLoading ? (
-          <ActivityIndicator size="large" color={COLORS.primary} />
-        ) : error ? (
-          <Text>Something went wrong</Text>
-        ) : (
-          <FlatList
-            data={data}
-            renderItem={({ item }) => (
-              <PopularJobCard
-                item={item}
-                selectedJob={selectedJob}
-                handleCardPress={handleCardPress}
-              />
-            )}
-            keyExtractor={(item) => item.job_id}
-            contentContainerStyle={{ columnGap: SIZES.medium }}
-            horizontal
+  useEffect(() => {
+    handleSearch()
+  }, [])
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerTitle: '',
+        }}
+      />
+
+      <FlatList
+        data={searchResult}
+        renderItem={({ item }) => (
+          <NearbyJobCard
+            job={item}
+            handleNavigate={() => router.push(`/job-details/${item.job_id}`)}
           />
         )}
-      </View>
-    </View>
+        keyExtractor={(item) => item.job_id}
+        contentContainerStyle={{ padding: SIZES.medium, rowGap: SIZES.medium }}
+        ListHeaderComponent={() => (
+          <>
+            <View style={styles.container}>
+              <Text style={styles.searchTitle}>{params.id}</Text>
+              <Text style={styles.noOfSearchedJobs}>Job Opportunities</Text>
+            </View>
+            <View style={styles.loaderContainer}>
+              {searchLoader ? (
+                <ActivityIndicator size="large" color={COLORS.primary} />
+              ) : (
+                searchError && <Text>Oops something went wrong</Text>
+              )}
+            </View>
+          </>
+        )}
+        ListFooterComponent={() => (
+          <View style={styles.footerContainer}>
+            <TouchableOpacity
+              style={styles.paginationButton}
+              onPress={() => handlePagination('left')}
+            >
+              <Image
+                source={icons.chevronLeft}
+                style={styles.paginationImage}
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+            <View style={styles.paginationTextBox}>
+              <Text style={styles.paginationText}>{page}</Text>
+            </View>
+            <TouchableOpacity
+              style={styles.paginationButton}
+              onPress={() => handlePagination('right')}
+            >
+              <Image
+                source={icons.chevronRight}
+                style={styles.paginationImage}
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
-export default Popularjobs
+export default JobSearch
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import {
   View,
   Text,
@@ -7,20 +7,21 @@ import {
   Image,
   FlatList,
 } from 'react-native'
-
 import { useRouter } from 'expo-router'
+
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants'
 
-const jobTypes = ['Full-Time', 'Part-Time', 'Contractor']
+const jobTypes = ['Full-time', 'Part-time', 'Contractor']
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter()
-  const [activeJobType, setActiveJobType] = useState('Full-Time')
+  const [activeJobType, setActiveJobType] = useState('Full-time')
+
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Geri</Text>
+        <Text style={styles.userName}>Hello Adrian</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
 
@@ -28,13 +29,13 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=""
-            onChange={() => {}}
-            placeholder={'What are you looking for?'}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder="What are you looking for?"
           />
         </View>
 
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image
             source={icons.search}
             resizeMode="contain"
